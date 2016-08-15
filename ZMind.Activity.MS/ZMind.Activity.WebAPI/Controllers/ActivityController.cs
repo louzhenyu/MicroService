eb5be4e7e72ff3b31ecf2da7efd5ed8c8fa6102a
娄振宇 <zhenyu.lou@zmind.cn>
2016-08-15 10:41:41 +0800

    a

---
+++ ZMind.Activity.MS/ZMind.Activity.WebAPI/Controllers/ActivityController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+using ZMind.Activity.Application;
+using ZMind.Activity.DTO.Response;
+
+namespace ZMind.Activity.WebAPI.Controllers
+{
+    public class ActivityController : ApiController
+    {
+        private ActivityApplication activityApp = new ActivityApplication();
+        // GET activity?activityId=
+        public ActivityResponse Get(string activityId)
+        {
+            var caller = User as ClaimsPrincipal;
+
+            var subjectClaim = caller.FindFirst("sub");
+            if (subjectClaim != null)
+            {
+                var test = caller.FindFirst("ClientId");
+            }
+            activityId = "5B4ABBFA-914C-463F-914E-ADBE21A81C70";
+            return activityApp.GetActivity(new Guid(activityId));
+        }
+    }
+}
---
+++ ZMind.Activity.MS/ZMind.Activity.WebAPI/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using IdentityServer3.AccessTokenValidation;
+using Microsoft.Owin;
+using Owin;
+[assembly: OwinStartup(typeof(ZMind.Activity.WebAPI.Startup))]
+namespace ZMind.Activity.WebAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseIdentityServerBearerTokenAuthentication(new IdentityServerBearerTokenAuthenticationOptions
+            {
+                Authority = "http://localhost:5000",
+                ValidationMode = ValidationMode.ValidationEndpoint,
+
+                RequiredScopes = new[] { "ActivityApi" }
+            });
+        }
+    }
+}
